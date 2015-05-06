62f60d10b7c8f82d20514c802683d1fa417bc95d
Tommy Tang <1530234656@qq.com>
2015-05-06 17:45:31 +0800

    add google analytics

--- master/js/controller/analysis.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?b0835d5e47a5960279d68390c2386f87";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
